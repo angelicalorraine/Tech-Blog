7431e72a76802bb6b601c329b7e5d2f0da89aa27
Angelica Hernandez <gel.lorraine@gmail.com>
2021-06-24 02:02:58 -0700

    id route and page updated - auth continued

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,9 +3,5 @@ module.exports = {
     // Format date as MM/DD/YYYY
     return date.toLocaleDateString();
   },
-  format_amount: (amount) => {
-    // format large numbers with commas
-    return parseInt(amount).toLocaleString();
-  },
 
 };
