22abd5abdb4f0b9d6ab152b2c4b15b10828bc057
Angelica Hernandez <gel.lorraine@gmail.com>
2021-07-27 00:46:26 -0700

    new post modal & post complete

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,22 +1,22 @@
 const newFormHandler = async (event) => {
     event.preventDefault();
+    const title = document.querySelector('#post-title').value.trim();
+    const content = document.querySelector('#post-content').value.trim();
 
-    const name = document.querySelector('#project-name').value.trim();
-    const description = document.querySelector('#project-desc').value.trim();
 
-    if (name && description) {
+    if (title && content) {
         const response = await fetch(`/api/posts`, {
             method: 'POST',
-            body: JSON.stringify({ name, description }),
+            body: JSON.stringify({ title, content }),
             headers: {
                 'Content-Type': 'application/json',
             },
         });
 
         if (response.ok) {
-            document.location.replace('/dashboard');
+            document.location.replace('/profile');
         } else {
-            alert('Failed to create project');
+            alert('Failed to create post.');
         }
     }
 };
