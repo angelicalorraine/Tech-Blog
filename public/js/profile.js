b0b7d52355773c5ee2bb950730daab05aeec6fb0
Angelica Hernandez <gel.lorraine@gmail.com>
2021-08-17 17:11:01 -0700

    updates

--- public/js/profile.js
+++ public/js/profile.js
@@ -42,5 +42,5 @@ document
     .addEventListener('submit', newFormHandler);
 
 document
-    .querySelector('.post-list')
+    .querySelector('.btn-danger')
     .addEventListener('click', delButtonHandler);
